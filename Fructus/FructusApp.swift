f549e20ca8943df40237f69f2d8f9d8a2516856b
MarcCDJ <marccruzdj@gmail.com>
2023-10-29 15:12:37 -0400

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Marc Cruz on 10/29/23.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
